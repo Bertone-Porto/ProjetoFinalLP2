fa4c382d0f113a636c782acd78856930d0020cb3
Victor Marques Nogueira <victormarques248@hotmail.com>
2025-07-08 18:07:55 -0300

    Create MainScanner.java

---
+++ src/gameforge/main/MainScanner.java
@@ -0,0 +1,17 @@
+package gameforge.main;
+
+import gameforge.model.*;
+import gameforge.service.*;
+import gameforge.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class MainScanner {
+    public static void main(String[] args) {
+        Sistema gameForge = new Sistema();
+        gameForge.executar(); //o main chama o método principal de execução
+    }
+}
